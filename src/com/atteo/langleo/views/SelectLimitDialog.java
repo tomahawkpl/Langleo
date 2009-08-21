bd432b64983e1f8b3bdb12672fb341cece97437d
Tomek Piotrowski <tomahawk@atteo.com>
2009-08-21 18:00:54 +0200

    Increasing new words limit, 'I know it' button

---
+++ src/com/atteo/langleo/views/SelectLimitDialog.java
@@ -0,0 +1,21 @@
+package com.atteo.langleo.views;
+
+import android.app.Dialog;
+import android.content.Context;
+
+import com.atteo.langleo.R;
+
+public class SelectLimitDialog extends Dialog {
+
+	public SelectLimitDialog(Context context) {
+		super(context);
+		setTitle(R.string.more_new_words);
+		setContentView(R.layout.increase_limit_dialog);
+		
+		
+		NumberPicker np = (NumberPicker) findViewById(R.id.increase_limit_dialog_picker);
+		np.setRange(1, 100);
+		np.setCurrent(5);
+	}
+
+}
